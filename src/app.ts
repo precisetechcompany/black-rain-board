7083041f0afd7c73290c0c69cc67c930d6716bc1
domiabis <dominick.abis@gmail.com>
2019-09-11 09:17:08 +0530

    teams added

--- src/app.ts
+++ src/app.ts
@@ -4,25 +4,50 @@ import * as mongoose from "mongoose";
 import { Config } from './config/config';
 require("./models/Board");
 require("./models/Task");
+require("./models/User");
+require("./models/Team");
+require("./models/Member");
 import { BaseRouter } from "./routes/base";
 import { BoardRouter } from "./routes/board";
 import { TaskRouter } from "./routes/Task";
+import {UserRouter} from "./routes/User";
+import { TeamRouter } from "./routes/Team";
+import { MemberRouter } from "./routes/Member";
 import BoardService = require("./services/BoardService");
 import TaskService = require("./services/TaskService");
+import UserService = require("./services/UserService");
+import TeamService = require("./services/TeamService");
+import MemberService = require("./services/MemberService");
 import BoardServiceImpl = require("./services/impl/BoardServiceImpl");
 import TaskServiceImpl = require("./services/impl/TaskServiceImpl");
+import UserServiceImpl = require("./services/impl/UserServiceImpl");
+import TeamServiceImpl = require("./services/impl/TeamServiceImpl");
+import MemberServiceImpl = require("./services/impl/MemberServiceImpl");
 import { BoardController } from "./controllers/BoardController";
 import { TaskController } from "./controllers/TaskController";
+import { UserController } from "./controllers/UserController";
+import { TeamController } from "./controllers/TeamController";
+import { MemberController } from "./controllers/MemberController";
 
 class App {
   public express:any = express.application;
   public baseRouter;
   boardService: BoardService.BoardService = new BoardServiceImpl.BoardServiceImpl();
   taskService: TaskService.TaskService = new TaskServiceImpl.TaskServiceImpl();
+  userService: UserService.UserService = new UserServiceImpl.UserServiceImpl();
+  teamService: TeamService.TeamService = new TeamServiceImpl.TeamServiceImpl();
+  memberService: MemberService.MemberService = new MemberServiceImpl.MemberServiceImpl();
   boardController : BoardController = new BoardController(this.boardService);
   taskController : TaskController = new TaskController(this.taskService);
+  userController : UserController = new UserController(this.userService);
+  teamController : TeamController = new TeamController(this.teamService);
+  memberController : MemberController = new MemberController(this.memberService);
   boardRouter: BoardRouter = new BoardRouter(this.boardController);
   taskRouter: TaskRouter = new TaskRouter(this.taskController);
+  userRouter: UserRouter = new UserRouter(this.userController);
+  teamRouter: TeamRouter = new TeamRouter(this.teamController);
+  memberRouter: MemberRouter = new MemberRouter(this.memberController);
+
 
   constructor () {
     this.mongoSetup();
@@ -32,6 +57,9 @@ class App {
     this.baseRouter.BaseRouter(this.express);
     this.boardRouter.BoardRouter(this.express);
     this.taskRouter.TaskRouter(this.express);
+    this.userRouter.UserRouter(this.express);
+    this.teamRouter.TeamRouter(this.express);
+    this.memberRouter.MemberRouter(this.express);
     this.mountRoutes()
   }
 
@@ -39,6 +67,9 @@ class App {
     this.express.use('/', this.baseRouter.router)
     this.express.use('/boards', this.boardRouter.router)
     this.express.use('/tasks', this.taskRouter.router)
+    this.express.use('/users', this.userRouter.router)
+    this.express.use('/teams', this.teamRouter.router)
+    this.express.use('/members', this.memberRouter.router)
   }
 
   private config(): void {
